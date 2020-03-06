2614ecbc2f0e2ce5b064d0cbff08e3a7596b1be6
Gregory Nail <gnail2@unl.edu>
2020-03-05 20:12:58 -0600

    built a shell homepage UI without css

--- valorant-stats-app/src/App.js
+++ valorant-stats-app/src/App.js
@@ -1,26 +1,96 @@
 import React from "react";
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+import {
+    Form,
+    FormGroup,
+    FormInput,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    Collapse,
+    Button
+} from "shards-react"
 import "./App.css";
 
-function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <img src={ logo } className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-                  Learn React
-              </a>
-          </header>
-      </div>
-  );
-}
+export default class NavExample extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggleNavbar = this.toggleNavbar.bind(this);
+  
+      this.state = {
+        dropdownOpen: false,
+        collapseOpen: false
+      };
+    }
+  
+    toggleNavbar() {
+      this.setState({
+        ...this.state,
+        ...{
+          collapseOpen: !this.state.collapseOpen
+        }
+      });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar type="dark" theme="primary" expand="md">
+                <NavbarBrand href="#">Valorant Stats App</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNavbar} />
+                <Collapse open={this.state.collapseOpen} navbar>
+                <Nav navbar>
+                    <NavItem>
+                    <NavLink active href="#">
+                        Home
+                    </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink href="#" >
+                        Live Game
+                    </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink href="#" >
+                        Tier Lists
+                    </NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink href="#" >
+                        Strategies
+                    </NavLink>
+                    </NavItem>
+                </Nav>
 
-export default App;
+                <Nav navbar className="ml-auto">
+                    <InputGroup size="sm" seamless>
+                    <InputGroupAddon type="prepend">
+                        <InputGroupText>
+                        </InputGroupText>
+                    </InputGroupAddon>
+                    <FormInput className="border-0" placeholder="Search for player..." />
+                    </InputGroup>
+                </Nav>
+                </Collapse>
+            </Navbar>
+            <Form>
+                <FormGroup>
+                    <FormInput placeholder="Enter player name here"/>
+                    <Button>Search</Button> 
+                </FormGroup>
+            </Form>
+        </div>
+    );
+}
+}
